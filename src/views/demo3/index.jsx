6f7792f37e6de8be9950308c35f0f0fa334844e2
libocai <1519497988@qq.com>
2022-05-26 18:00:15 +0800

    467

--- src/views/demo3/index.jsx
+++ src/views/demo3/index.jsx
@@ -1,17 +1,24 @@
 import React, { Component } from 'react'
-import { Link, Route, Routes } from 'react-router-dom'
+import { Link, Route, Routes, useNavigate } from 'react-router-dom'
+
 import Home from './home'
 import Router1 from './router1'
 import Router2 from './router2'
 
 export default class index extends Component {
+
+
+    goPush = () => {
+        let navigate = useNavigate()
+        navigate('/router1')
+    }
     render() {
         return (
             <div className='header'>
                 <div className='contanner' style={{ display: "flex" }}>
                     <div className='route-item' style={{ display: "flex", flexDirection: "column" }} >
                         <Link className='route-item-list' to="/router1">router1</Link>
-                        <Link className='route-item-list' to="/router2">router2</Link>
+                        <Link className='route-item-list' to="/router2" >router2</Link>
                     </div>
                     <div className='router-view' style={{ margin: "0 20px" }}>
                         <Routes>
@@ -20,6 +27,8 @@ export default class index extends Component {
                             <Route path="*" element={<Home />} />
                         </Routes>
                     </div>
+                    <button onClick={this.goPush}>跳转到Router1</button>
+                    <button>跳转到Router2</button>
                 </div>
             </div >
         )
--- src/views/demo3/router1.jsx
+++ src/views/demo3/router1.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 
 export default class router1 extends Component {
+    componentDidMount() {
+        console.log(this.props);
+    }
     render() {
         return (
             <div>我是router1</div>
